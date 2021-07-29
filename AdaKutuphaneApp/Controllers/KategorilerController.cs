d7e1ba9c4b06472faf7e56f622f3e6cd418905e8
ibrahim batı <ibrahimbati@gmail.com>
2021-07-29 11:55:42 +0300

    Kategoriler sayfası Listeleme yapıldı Butonlar eklendi

--- AdaKutuphaneApp/Controllers/KategorilerController.cs
+++ AdaKutuphaneApp/Controllers/KategorilerController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AdaKutuphaneApp.Models.Entity;
 
 namespace AdaKutuphaneApp.Controllers
 {
     public class KategorilerController : Controller
     {
         // GET: Kategoriler
+        AdaKutuphaneEntities1 db = new AdaKutuphaneEntities1();
         public ActionResult Index()
         {
-            return View();
+            var kategoriler = db.tblKategoriler.ToList();
+            return View(kategoriler);
         }
     }
 }
