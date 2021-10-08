6036143509b92d295e4bf7259045135e9382907a
ibrahim batı <ibrahimbati@gmail.com>
2021-10-08 17:44:19 +0300

    Ödnüç Verme

--- AdaKutuphaneApp/Controllers/OduncController.cs
+++ AdaKutuphaneApp/Controllers/OduncController.cs
@@ -3,20 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AdaKutuphaneApp.Models.Entity;
 
 namespace AdaKutuphaneApp.Controllers
 {
     public class OduncController : Controller
     {
         // GET: Odunc
+        AdaKutuphaneEntities1 db = new AdaKutuphaneEntities1();
         public ActionResult Index()
         {
-            return View();
+            var hareketler = db.tblHareketler.ToList();
+            return View(hareketler);
         }
 
+        [HttpGet]
         public ActionResult OduncVer()
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult OduncVer(tblHareketler h)
+        {
+            db.tblHareketler.Add(h);
+            db.SaveChanges();
+            return View();        
+        }
+
+
     }
 }
