3d28a751a78a40e4de7f6e37b557de7ba89df750
ibrahim batı <ibrahimbati@gmail.com>
2021-10-22 10:15:53 +0300

    İstatistikler sayfası başladı

---
+++ AdaKutuphaneApp/Controllers/IstatistiklerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AdaKutuphaneApp.Controllers
+{
+    public class IstatistiklerController : Controller
+    {
+        // GET: Istatistikler
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- AdaKutuphaneApp/Controllers/VitrinController.cs
+++ AdaKutuphaneApp/Controllers/VitrinController.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AdaKutuphaneApp.Models.Entity;
 
 namespace AdaKutuphaneApp.Controllers
 {
     public class VitrinController : Controller
     {
         // GET: Vitrin
+        AdaKutuphaneEntities1 db = new AdaKutuphaneEntities1();
         public ActionResult Index()
         {
-            return View();
+            var kitaplar = db.tblKitaplar.ToList();
+
+            return View(kitaplar);
         }
     }
 }
