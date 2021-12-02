9a0dca7d68f039d7a93ce0ecc142f850c3beab32
ibrahim batı <ibrahimbati@gmail.com>
2021-12-02 12:51:00 +0300

    Öğrenci panel

--- AdaKutuphaneApp/Controllers/KayitOlController.cs
+++ AdaKutuphaneApp/Controllers/KayitOlController.cs
@@ -3,15 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AdaKutuphaneApp.Models.Entity;
 
 namespace AdaKutuphaneApp.Controllers
 {
     public class KayitOlController : Controller
     {
         // GET: KayitOl
+        AdaKutuphaneEntities1 db = new AdaKutuphaneEntities1();
+        [HttpGet]
         public ActionResult Kayit()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult Kayit(tblUyeler u)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            db.tblUyeler.Add(u);
+            db.SaveChanges();
+            return View();
+            
         }
     }
 }
