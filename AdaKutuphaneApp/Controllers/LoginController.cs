f327467a424a7e12a20a6089ae79fb7c77597af7
ibrahim batı <ibrahimbati@gmail.com>
2021-12-11 23:37:06 +0300

    Profil güncelleme

--- AdaKutuphaneApp/Controllers/LoginController.cs
+++ AdaKutuphaneApp/Controllers/LoginController.cs
@@ -3,15 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AdaKutuphaneApp.Models.Entity;
+using System.Web.Security;
 
 namespace AdaKutuphaneApp.Controllers
 {
     public class LoginController : Controller
     {
         // GET: Login
+        AdaKutuphaneEntities1 db = new AdaKutuphaneEntities1();
         public ActionResult GirisYap()
         {
             return View();
         }
+        [HttpPost]
+        public ActionResult GirisYap(tblUyeler p)
+        {
+            var uyebilgisi = db.tblUyeler.FirstOrDefault(x => x.MAIL == p.MAIL && x.PAROLA == p.PAROLA);
+            if (uyebilgisi != null)
+            {
+                FormsAuthentication.SetAuthCookie(uyebilgisi.MAIL, false);  
+                Session["Mail"] = uyebilgisi.MAIL.ToString();
+                // Değiştirdik, alttakiler iptal
+                //TempData["Ad"] = uyebilgisi.AD.ToString();
+                //TempData["Soyad"] = uyebilgisi.SOYAD.ToString();
+                //TempData["Kullanici"] = uyebilgisi.KULLANICIADI.ToString();
+                //TempData["Sifre"] = uyebilgisi.PAROLA.ToString();
+                return RedirectToAction("Index", "OgrenciPaneli");
+            }
+            else
+            {
+                return View();
+            }
+
+        }
+
     }
 }
--- AdaKutuphaneApp/Controllers/OgrenciPaneliController.cs
+++ AdaKutuphaneApp/Controllers/OgrenciPaneliController.cs
@@ -3,14 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AdaKutuphaneApp.Models.Entity;
+
 
 namespace AdaKutuphaneApp.Controllers
 {
     public class OgrenciPaneliController : Controller
     {
         // GET: OgrenciPaneli
+        AdaKutuphaneEntities1 db = new AdaKutuphaneEntities1();
+        [HttpGet]
+        [Authorize]
         public ActionResult Index()
         {
+            var uyemail = (string)Session["Mail"];
+            var bilgiler = db.tblUyeler.FirstOrDefault(x => x.MAIL == uyemail);
+            return View(bilgiler);
+        }
+        [HttpPost]
+        public ActionResult Index(tblUyeler p)
+        {
+            var kullanici = (string)Session["Mail"];
+            var uye = db.tblUyeler.FirstOrDefault(x => x.MAIL == kullanici);
+            uye.PAROLA = p.PAROLA;
+            db.SaveChanges();
             return View();
         }
     }
